1632480e69f23484b5876f51af2c7fc7721a9bff
Thiago Wernz <t.wernz@hotmail.com>
2025-11-06 16:02:38 -0300

    Atualizando CORS para domínio público

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ app = FastAPI(
 
 # ----------------- CORS -----------------
 origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:8000",
+    #"http://localhost:5173",
+    #"http://127.0.0.1:5173",
+    #"http://127.0.0.1:8000",
     "https://meatburger.com.py",      # ✅ domínio da Hostinger
     "https://www.meatburger.com.py",  # ✅ versão com www
 ]
