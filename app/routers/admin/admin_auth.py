4bdfbd5b8dc5793578c1e1b8e528af19319a759c
Thiago Wernz <t.wernz@hotmail.com>
2025-11-15 23:47:33 -0300

    Atualizações no admin_auth e ajustes gerais

--- app/routers/admin/admin_auth.py
+++ app/routers/admin/admin_auth.py
@@ -5,14 +5,17 @@ from app.schemas.admin.admin_schema import AdminLogin, AdminResponse
 from app.models.admin.admin import Admin
 from app.utils.security import verify_password, create_access_token
 
-router = APIRouter(tags=["Admin Auth"])
+router = APIRouter(
+    prefix="/admin/auth",
+    tags=["Admin Auth"]
+)
 
 @router.post("/login", response_model=AdminResponse)
 def login_admin(data: AdminLogin, db: Session = Depends(get_db)):
     """
-    Login do admin -> POST /admin/login
-    Retorna: id, username, email, access_token, token_type
+    Login do admin -> POST /admin/auth/login
     """
+
     admin = db.query(Admin).filter(Admin.username == data.username).first()
 
     if not admin or not verify_password(data.password, admin.password):
@@ -20,7 +23,6 @@ def login_admin(data: AdminLogin, db: Session = Depends(get_db)):
 
     access_token = create_access_token({"sub": admin.username})
 
-    # Retorna access_token (nome compatível com frontend)
     return AdminResponse(
         id=admin.id,
         username=admin.username,
@@ -29,6 +31,10 @@ def login_admin(data: AdminLogin, db: Session = Depends(get_db)):
         token_type="bearer"
     )
 
+
 @router.get("/check")
 def check_status():
+    """
+    Verifica se a autenticação está funcionando -> GET /admin/auth/check
+    """
     return {"status": "Auth ativo e rodando 🚀"}
